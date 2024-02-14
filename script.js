434c0d6396e03c105f669a150c1c6f22f98f51cb
Sophie <123798482+spacefriend@users.noreply.github.com>
2024-02-14 17:11:19 -0500

    added randomized colours to books, greyscale to unread books, fixed issue when exiting add book popup would add an empty book to myLibrary array, added thickness of books based on page count

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookColoursRead = ['red', 'blue', 'purple', 'green', 'yellow'];
 
 function Book(title, author, pages, readStatus){
     this.title = title;
@@ -8,32 +9,35 @@ function Book(title, author, pages, readStatus){
 
 }
 
-myLibrary[0] = new Book('Mistborn', 'Brandon Sanderson', '100', 'Unread');
-myLibrary[1] = new Book('Rage of Dragons', 'BEvan Winter', '100', 'Unread');
-myLibrary[2] = new Book('Red Rising', 'Pierce Brown', '100', 'Read');
+myLibrary[0] = new Book('The Final Empire', 'Brandon Sanderson', '537', 'unread');
+myLibrary[1] = new Book('The Rage of Dragons', 'Evan Winter', '535', 'unread');
+myLibrary[2] = new Book('Red Rising', 'Pierce Brown', '382', 'read');
 
-let addBookForm = document.getElementById("addBookForm");
-let addBookBtn = document.getElementById("addBookBtn");
-let exitAddBookBtn = document.getElementById("exit");
+let addBookForm = document.getElementById('addBookForm');
+let addBookBtn = document.getElementById('addBookBtn');
+let exitAddBookBtn = document.getElementById('exit');
 let bookCount = 3;
 
-addBookBtn.addEventListener("click", (e) => {
+addBookBtn.addEventListener('click', (e) => {
     document.getElementById('hidden').id = 'visible';
 })
 
-exitAddBookBtn.addEventListener("click", (e) => {
-
+exitAddBookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     document.getElementById('visible').id = 'hidden';
     
 })
 
-addBookForm.addEventListener("submit", (e) => {
+/* 
+Creates book object using user input, adds the object to the myLibrary array, and displays the new book
+*/
+addBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let readStatus = addBookForm.elements["readStatus"].value
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let readStatus = addBookForm.elements['readStatus'].value
     
     const newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook);
@@ -47,23 +51,67 @@ function addBookToLibrary(){
 
 }
 
-
+/*
+Function for displaying a newly added book
+*/
 function displayAddedBook(){
-    const shelf = document.getElementById("shelf");
+    const shelf = document.getElementById('shelf');
     const addBook = document.createElement('div');
 
-    addBook.innerHTML = `<div class='book'>` + myLibrary[bookCount].title + `</div>`;
+    let colour = bookColoursRead[Math.floor(Math.random()*4)];
+    bookColoursRead.push(bookColoursRead.splice(bookColoursRead.indexOf(colour), 1)[0]);
+
+    let readStatusColour = '';
+    if (myLibrary[bookCount].readStatus == 'unread'){
+        readStatusColour = 'unread';
+    }
+
+    let bookLength = 'short';
+
+    if (myLibrary[bookCount].pages <= 300){
+        bookLength = 'short';
+    }else if(myLibrary[bookCount].pages <= 450 ){
+        bookLength = 'medium'
+    }else if(myLibrary[bookCount].pages <= 600){
+        bookLength = 'long'
+    }else{
+        bookLength = 'xlong'
+    }
+
+    addBook.innerHTML = `<div class='book ` + bookLength +` `+ colour + ` `+ readStatusColour + `'><span>` + myLibrary[bookCount].title + `</span></div>`;
     shelf.appendChild(addBook);
     bookCount++;
 
 }
+/*
+Function to display initial books already in the array
+*/
 
 function initialDisplayBook(){
-   const shelf = document.getElementById("shelf");
+   const shelf = document.getElementById('shelf');
     for (book of myLibrary){
         console.log(book);
         const newBook = document.createElement('div');
-        newBook.innerHTML = `<div class='book'>` + book.title + `</div>`;
+
+        let readStatusColour = '';
+        if (book.readStatus == 'unread'){
+            readStatusColour = 'unread';
+        }
+
+        let colour = bookColoursRead[Math.floor(Math.random()*4)];
+        bookColoursRead.push(bookColoursRead.splice(bookColoursRead.indexOf(colour), 1)[0]);
+        let bookLength = 'short';
+
+        if (book.pages <= 300){
+            bookLength = 'short';
+        }else if(book.pages <= 450 ){
+            bookLength = 'medium'
+        }else if(book.pages <= 600){
+            bookLength = 'long'
+        }else{
+            bookLength = 'xlong'
+        }
+        newBook.innerHTML = `<div class='book ` + bookLength +` `+ colour + ` `+ readStatusColour + `'><span>` + book.title + `</span></div>`;
         shelf.appendChild(newBook);
         
     }
